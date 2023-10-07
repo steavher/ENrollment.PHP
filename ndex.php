346eb2df7b3026b6d8821e65b71ae5afad790f05
steavher <97264361+steavher@users.noreply.github.com>
2023-10-07 16:33:26 +0800

    Update and rename login.php to ndex.php

---
+++ ndex.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+
+    <form method="POST" action="login.php">
+    	<div class="login-container">
+            <img src="LOGO.png">
+    		
+        <h1>LOGIN</h1>
+ 				
+ 				<form>
+ 					<input type="text" placeholder="Username" required>
+ 					<input type="password" placeholder="Password" required>
+ 				</form> 
+            <button type="Submit">Login</button> <br>
+            <p> Don't have an account? <a href="Register.php">Register</a>
+        </div>
+    </form>
+
+</body>
+</html>
