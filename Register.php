211ebae7fa7059871aef0ff3cf4fd5d059a731c6
steavher <97264361+steavher@users.noreply.github.com>
2023-10-09 13:31:33 +0800

    Update Register.php

--- Register.php
+++ Register.php
@@ -5,22 +5,31 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>Register Form</title>
 
-	<link rel="stylesheet" href="css/style.css">
+	<link rel="stylesheet" href="CSS/Register.css">
 </head>
 <body>
 
-<div class="form-container">
-	
 	<form action="Register.php" method="post">
-		<h3>Register Now</h3>
+		<div class="form-container">
+
+		<?php 
+		 if (isset($error)) {
+            foreach ($error as $error) {
+                echo '<span class="error-msg">' . $error . '</span>';
+            }
+        }
+        ?>
+	
+		<h1>Register Now</h1><br><select name="user_type">
+			<option value="user">User</option>
+			<option value="admin">Admin</option>
+		</select> <br>
 		<input type="text" name="name" required placeholder="Enter your Name">
 		<input type="email" name="email" required placeholder="Enter your email">
 		<input type="password" name="password" required placeholder="Enter your password">
 		<input type="password" name="cspassword" required placeholder="Confirm your password">
-		<select name="user_type">
-			<option value="user">User</option>
-			<option value="admin">Admin</option>
-		</select>
+		<button type="submit">Register</button>
+		<p>Already have an account? <a href="ndex.php">Sign In</a></p>
 	</form>
 </div>
 </body>
