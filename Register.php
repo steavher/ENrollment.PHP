9f1cca440b7dfaab631488e358e13ce01f5c8f31
steavher <97264361+steavher@users.noreply.github.com>
2023-11-18 19:11:09 +0800

    Update Register.php

--- Register.php
+++ Register.php
@@ -1,10 +1,10 @@
 <?php
-session_start(); // Start the session
+session_start();
 
 $errors = [];
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $user_type = "Student"; // Set user type to "Student" for every registration
+    $user_type = "Student";
     $email = $_POST['email'];
     $password = $_POST['password'];
     $cspassword = $_POST['cspassword'];
@@ -18,19 +18,14 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     }
 
     if (empty($errors)) {
-        // Hash the password for security
         $hashed_password = password_hash($password, PASSWORD_DEFAULT);
-
-        // Generate a verification code (OTP)
         $verify_code = mt_rand(100000, 999999);
 
-        // Insert user data into the database (replace with your database connection code)
         $conn = new mysqli("localhost", "root", "", "phs_enrollment");
         if ($conn->connect_error) {
             die("Connection failed: " . $conn->connect_error);
         }
 
-        // Check if the email already exists in the database
         $check_existing_email = $conn->prepare("SELECT email FROM accounts WHERE email = ?");
         $check_existing_email->bind_param("s", $email);
         $check_existing_email->execute();
@@ -39,16 +34,13 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         if ($check_existing_email->num_rows > 0) {
             $errors[] = "Email already exists";
         } else {
-            // If the email is not found in the database, proceed with the registration
             $stmt = $conn->prepare("INSERT INTO accounts (user_type, email, password, verify_code, verify) VALUES (?, ?, ?, ?, 0)");
             $stmt->bind_param("sssi", $user_type, $email, $hashed_password, $verify_code);
 
             if ($stmt->execute()) {
-                // Send the verification email
-                include 'verification.php'; // Include the verification script
+                $_SESSION['user_email'] = $email; // Set the user's email in the session
+                include 'verification.php';
                 sendemail_verify($email, $verify_code);
-
-                // Redirect to a verification page (verification.php)
                 header("Location: verification.php");
                 exit();
             } else {
@@ -70,7 +62,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Register Form</title>
-    <link rel="stylesheet" href="Register.css">
+    <link rel="stylesheet" href="css/Register.css">
     <style>
         .error-block {
             background-color: #ff6666;
@@ -84,7 +76,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 <body>
     <form action="register.php" method="post">
         <div class="form-container">
-        <?php 
+            <?php 
             if (!empty($errors)) {
                 echo '<div class="error-block">';
                 foreach ($errors as $errorMsg) {
@@ -93,12 +85,13 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                 echo '</div>';
             }
             ?>
+            <img src="IMAGES/LOGO.png">
             <h1>Register Now</h1><br>
             <input type="email" name="email" required placeholder="Enter your email">
             <input type="password" name="password" required placeholder="Enter your password">
             <input type="password" name="cspassword" required placeholder="Confirm your password">
             <button type="submit">Register</button>
-            <p>Already have an account? <a href="ndex.php">Sign In</a></p>
+            <p>Already have an account? <a href="index.php">Sign In</a></p>
         </div>
     </form>
 </body>
