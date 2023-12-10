924237c23537b3a5d70f02b2f4f7903477da988e
steavher <97264361+steavher@users.noreply.github.com>
2023-12-10 17:23:25 +0800

    Update delete_admin.php

--- delete_admin.php
+++ delete_admin.php
@@ -12,22 +12,31 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-// Check if Admin type is provided in the request
-if (isset($_GET['Admin'])) {
+if (isset($_GET['account_Id'])) {
+    // Get the adminId from the $_GET parameters
+    $adminId = $_GET['account_Id'];
 
-    $deleteQuery = "DELETE FROM accounts WHERE user_type = 'Admin'";
+    // Check if the adminId is a valid integer
+    if (!is_numeric($adminId)) {
+        $_SESSION['error_message'] = "Invalid admin ID.";
+        header("Location: USERS.php");
+        exit();
+    }
+
+    // Prepare and execute the SQL query to delete the admin user
+    $deleteQuery = "DELETE FROM accounts WHERE id = $adminId";
 
     if ($conn->query($deleteQuery) === TRUE) {
-        $_SESSION['success_message'] = "Admin user(s) deleted successfully.";
+        $_SESSION['success_message'] = "Admin user deleted successfully.";
     } else {
-        $_SESSION['error_message'] = "Error deleting admin user(s): " . $conn->error;
+        $_SESSION['error_message'] = "Error deleting admin user: " . $conn->error;
     }
 
     // Redirect back to the page where the deletion was initiated
     header("Location: USERS.php");
     exit();
 } else {
-    $_SESSION['error_message'] = "Admin type not provided.";
+    $_SESSION['error_message'] = "Admin ID not provided.";
     header("Location: USERS.php");
     exit();
 }
