77c0ed626560e7ced1488f23e38372b5c2dd965f
steavher <97264361+steavher@users.noreply.github.com>
2024-01-12 08:09:16 +0800

    Update sidebar.php

--- sidebar.php
+++ sidebar.php
@@ -6,7 +6,8 @@
                 <li><a href="admin_dashboard.php"><i class="fas fa-home"></i><span>DASHBORD</span></a></li>
                 <li><a href="STUDENTS.php"><i class="fas fa-user"></i><span>STUDENTS</span></a></li>
                 <li><a href="USERS.php"><i class="fas fa-address-card"></i><span>USERS</span></a></li>
-<!--                 <li><a href="USER_PROFILE.php"><i class="fas fa-project-diagram"></i></span>USER PROFILE</span></a></li> -->
+                <li><a href="classlist.php"><i class="fas fa-address-card"></i><span>CLASS LIST</span></a></li>
+                <!-- <li><a href="USER_PROFILE.php"><i class="fas fa-project-diagram"></i></span>USER PROFILE</span></a></li> -->
                 <li><a href="../FINALIZED/ndex.php"><i class="fas fa-lock-open"></i><span>LOGOUT</span></a></li>
             </ul>
         </div>
